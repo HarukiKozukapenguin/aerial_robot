c3326a213e68c39f3cec71168800058aaa9e0fd7
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2023-05-01 23:05:08 +0900

    make bulb in rviz simulator

--- robots/dragon/src/visualize/light_bulb.cpp
+++ robots/dragon/src/visualize/light_bulb.cpp
@@ -17,51 +17,45 @@ int main(int argc, char** argv)
   while (ros::ok())
   {
     visualization_msgs::MarkerArray marker_array;
-    marker_array.markers.resize(3);
+    marker_array.markers.resize(2);
 
     visualization_msgs::Marker marker;
     marker.header.frame_id = "dragon/root";
     marker.header.stamp = ros::Time::now();
     marker.ns = "basic_shapes";
-    marker.type = visualization_msgs::Marker::CUBE;
     marker.action = visualization_msgs::Marker::ADD;
     marker.lifetime = ros::Duration();
-    marker.pose.orientation.x = -0.0740883274;
-    marker.pose.orientation.y = 0.1490180347;
-    marker.pose.orientation.z = 0.0111973555;
-    marker.pose.orientation.w = 0.9859914626;
-    marker.color.r = 0.392f;
-    marker.color.g = 0.251f;
-    marker.color.b = 0.204f;
+    marker.pose.orientation.x = -0.1947091712;
+    marker.pose.orientation.y = 0.1947091712;
+    marker.pose.orientation.z = 0.0394695030;
+    marker.pose.orientation.w = 0.9605304970;
     marker.color.a = 1.0f;
 
     marker.id = 0;
-    marker.scale.x = 0.25;
-    marker.scale.y = 0.05;
-    marker.scale.z = 2.0;
-    marker.pose.position.x = 0.4;
-    marker.pose.position.y = 0.2;
-    marker.pose.position.z = 0.7;
+    marker.type = visualization_msgs::Marker::CYLINDER;
+    marker.scale.x = 0.2;
+    marker.scale.y = 0.2;
+    marker.scale.z = 0.3;
+    marker.pose.position.x = 0.6;
+    marker.pose.position.y = 0.3;
+    marker.pose.position.z = 0.9;
+    marker.color.r = 1.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 1.0f;
     marker_array.markers[0] = marker;
     
     marker.id = 1;
-    marker.scale.x = 0.05;
-    marker.scale.y = 0.25;
-    marker.scale.z = 2.0;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.scale.x = 0.6;
+    marker.scale.y = 0.6;
+    marker.scale.z = 0.6;
     marker.pose.position.x = 0.5;
     marker.pose.position.y = 0.2;
     marker.pose.position.z = 0.7-0.0308;
+    marker.color.r = 1.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.50f;
     marker_array.markers[1] = marker;
-
-    marker.id = 2;
-    marker.scale.x = 0.05;
-    marker.scale.y = 0.25;
-    marker.scale.z = 2.0;
-    marker.pose.position.x = 0.3;
-    marker.pose.position.y = 0.2;
-    marker.pose.position.z = 0.7+0.0308;
-    marker_array.markers[2] = marker;
-
 
     marker_pub.publish(marker_array);
 
