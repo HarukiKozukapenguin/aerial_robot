dacfa6706e84aaa9d3e7e09a5abacb611956944e
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2023-05-11 22:59:43 +0900

    [DRAGON][gakushin_fig] set steel for new fig

--- robots/dragon/src/visualize/steel.cpp
+++ robots/dragon/src/visualize/steel.cpp
@@ -26,40 +26,35 @@ int main(int argc, char** argv)
     marker.type = visualization_msgs::Marker::CUBE;
     marker.action = visualization_msgs::Marker::ADD;
     marker.lifetime = ros::Duration();
-    marker.pose.orientation.x = -0.0740883274;
-    marker.pose.orientation.y = 0.1490180347;
-    marker.pose.orientation.z = 0.0111973555;
-    marker.pose.orientation.w = 0.9859914626;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
     marker.color.r = 0.392f;
     marker.color.g = 0.251f;
     marker.color.b = 0.204f;
     marker.color.a = 1.0f;
 
+    marker.scale.z = 2.0;
+    marker.pose.position.y = 0.3;
+    marker.pose.position.z = -0.3;
+
     marker.id = 0;
     marker.scale.x = 0.25;
     marker.scale.y = 0.05;
-    marker.scale.z = 2.0;
-    marker.pose.position.x = 0.4;
-    marker.pose.position.y = 0.2;
-    marker.pose.position.z = 0.7;
+    marker.pose.position.x = -0.05;
     marker_array.markers[0] = marker;
     
     marker.id = 1;
     marker.scale.x = 0.05;
     marker.scale.y = 0.25;
-    marker.scale.z = 2.0;
-    marker.pose.position.x = 0.5;
-    marker.pose.position.y = 0.2;
-    marker.pose.position.z = 0.7-0.0308;
+    marker.pose.position.x = -0.15;
     marker_array.markers[1] = marker;
 
     marker.id = 2;
     marker.scale.x = 0.05;
     marker.scale.y = 0.25;
-    marker.scale.z = 2.0;
-    marker.pose.position.x = 0.3;
-    marker.pose.position.y = 0.2;
-    marker.pose.position.z = 0.7+0.0308;
+    marker.pose.position.x = 0.05;
     marker_array.markers[2] = marker;
 
 
