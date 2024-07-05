80c735c08d8860b57dc78daa5da48614862e84b1
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-07-05 18:58:20 +0900

    [aerial_robot_base][debug] calc RSME of multiple obstacle in one rosbag

--- aerial_robot_base/scripts/rms.py
+++ aerial_robot_base/scripts/rms.py
@@ -18,26 +18,25 @@ h: stop the calculate the output the RMS results.
 class TreeDb:
         shift_x = -2.0
         shift_y = -0.25
-        def __init__(pos_x, pos_y, radius, marker_id):
-                self.pos_x:float = pos_x + shift_x
-                self.pos_y:float = pos_y + shift_y
+        
+        def __init__(self, pos_x, pos_y, radius, marker_id):
+                self.pos_x:float = pos_x + TreeDb.shift_x
+                self.pos_y:float = pos_y + TreeDb.shift_y
                 self.radius:float = radius
                 self.marker_id:int = marker_id
         
 def cb(data):
         global start_flag
+        global pose_cnt
 
         if start_flag == True:
-                global pose_cnt
-                global tree_squared_errors_sum_list
                 pose_cnt = pose_cnt + 1
 
-                for i, tree_db in emurate(tree_db_list):
+                for i, tree_db in enumerate(tree_db_list):
                         true_tree_pos_x = tree_db.pos_x
                         true_tree_pos_y = tree_db.pos_y
                         true_tree_radius = tree_db.radius
                         marker_id = tree_db.marker_id
-                        
                         error_tree_pos_x = data.markers[marker_id].pose.position.x - true_tree_pos_x
                         error_tree_pos_y = data.markers[marker_id].pose.position.y - true_tree_pos_y
                         error_tree_radius = float(data.markers[marker_id].text) - true_tree_radius
@@ -59,17 +58,19 @@ def getKey():
 if __name__=="__main__":
         settings = termios.tcgetattr(sys.stdin)
 
+        global tree_db_list
+        global tree_squared_errors_sum_list
+        global pose_cnt
+
         start_flag = False
         msg_cnt = 0
 
         pose_cnt = 0
-
-        global tree_db_list
-        global tree_squared_errors_sum_list
+        
         tree_db_list = []
-        tree_db_list.append(TreeDb(1.2,0.5,0.25,0))
+        tree_db_list.append(TreeDb(1.2,0.5,0.25,4))
         tree_db_list.append(TreeDb(2.5,-0.9,0.25,0))
-        tree_squared_errors_sum_list = [[0] * 3] * len(tree_db_list)
+        tree_squared_errors_sum_list = [[0] * 3 for _ in range(len(tree_db_list))]
 
 
         rospy.Subscriber("/multirotor/visualization_marker", MarkerArray, cb)
@@ -86,7 +87,8 @@ if __name__=="__main__":
                                 start_flag = True
                         if key == 'h':
                                 rospy.loginfo("stop calculation")
-                                for j, tree_squared_erros_sum in emurate(tree_squared_errors_sum_list):
+
+                                for j, tree_squared_errors_sum in enumerate(tree_squared_errors_sum_list):
                                         rms = [0] * 3
                                         if pose_cnt > 0:
                                                 rms = [math.sqrt(i / pose_cnt) for i in tree_squared_errors_sum]
