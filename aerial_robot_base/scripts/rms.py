11158ecead1c98f09bc2d0db2b3aeb0e9fe3796e
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-07-05 17:47:38 +0900

    [WIP] init version of calc RMSE of several tree

--- aerial_robot_base/scripts/rms.py
+++ aerial_robot_base/scripts/rms.py
@@ -15,28 +15,38 @@ s: start to subscribe the topic regarding to control errors, and start to calcul
 h: stop the calculate the output the RMS results.
 """
 
+class TreeDb:
+        shift_x = -2.0
+        shift_y = -0.25
+        def __init__(pos_x, pos_y, radius, marker_id):
+                self.pos_x:float = pos_x + shift_x
+                self.pos_y:float = pos_y + shift_y
+                self.radius:float = radius
+                self.marker_id:int = marker_id
+        
 def cb(data):
         global start_flag
 
-        true_tree_pos_x = -0.2
-        true_tree_pos_y = -0.15
-        true_tree_radius = 0.25
-
-        marker_id = 2
         if start_flag == True:
                 global pose_cnt
-                global tree_squared_errors_sum
+                global tree_squared_errors_sum_list
                 pose_cnt = pose_cnt + 1
 
-                error_tree_pos_x = data.markers[marker_id].pose.position.x - true_tree_pos_x
-                error_tree_pos_y = data.markers[marker_id].pose.position.y - true_tree_pos_y
-                error_tree_radius = float(data.markers[marker_id].text) - true_tree_radius
+                for i, tree_db in emurate(tree_db_list):
+                        true_tree_pos_x = tree_db.pos_x
+                        true_tree_pos_y = tree_db.pos_y
+                        true_tree_radius = tree_db.radius
+                        marker_id = tree_db.marker_id
+                        
+                        error_tree_pos_x = data.markers[marker_id].pose.position.x - true_tree_pos_x
+                        error_tree_pos_y = data.markers[marker_id].pose.position.y - true_tree_pos_y
+                        error_tree_radius = float(data.markers[marker_id].text) - true_tree_radius
 
-                tree_squared_errors_sum[0] = tree_squared_errors_sum[0] + error_tree_pos_x * error_tree_pos_x
-                tree_squared_errors_sum[1] = tree_squared_errors_sum[1] + error_tree_pos_y * error_tree_pos_y
-                tree_squared_errors_sum[2] = tree_squared_errors_sum[2] + error_tree_radius * error_tree_radius
+                        tree_squared_errors_sum_list[i][0] = tree_squared_errors_sum_list[i][0] + error_tree_pos_x * error_tree_pos_x
+                        tree_squared_errors_sum_list[i][1] = tree_squared_errors_sum_list[i][1] + error_tree_pos_y * error_tree_pos_y
+                        tree_squared_errors_sum_list[i][2] = tree_squared_errors_sum_list[i][2] + error_tree_radius * error_tree_radius
 
-                rms = [math.sqrt(i / pose_cnt) for i in tree_squared_errors_sum]
+                        # rms = [math.sqrt(i / pose_cnt) for i in tree_squared_errors_sum]
                 #rospy.loginfo("RMS errors of pos: [%f, %f, %f]; rot: [%f, %f, %f]", rms[0], rms[1], rms[2], rms[3], rms[4], rms[5])
 
 def getKey():
@@ -52,8 +62,15 @@ if __name__=="__main__":
         start_flag = False
         msg_cnt = 0
 
-        tree_squared_errors_sum = [0] * 3
         pose_cnt = 0
+
+        global tree_db_list
+        global tree_squared_errors_sum_list
+        tree_db_list = []
+        tree_db_list.append(TreeDb(1.2,0.5,0.25,0))
+        tree_db_list.append(TreeDb(2.5,-0.9,0.25,0))
+        tree_squared_errors_sum_list = [[0] * 3] * len(tree_db_list)
+
 
         rospy.Subscriber("/multirotor/visualization_marker", MarkerArray, cb)
 
@@ -69,17 +86,17 @@ if __name__=="__main__":
                                 start_flag = True
                         if key == 'h':
                                 rospy.loginfo("stop calculation")
+                                for j, tree_squared_erros_sum in emurate(tree_squared_errors_sum_list):
+                                        rms = [0] * 3
+                                        if pose_cnt > 0:
+                                                rms = [math.sqrt(i / pose_cnt) for i in tree_squared_errors_sum]
 
-                                rms = [0] * 3
-                                if pose_cnt > 0:
-                                        rms = [math.sqrt(i / pose_cnt) for i in tree_squared_errors_sum]
-
-                                rospy.loginfo("RMS of tree_pos errors: [%f, %f], radius_errors: [%f]", rms[0], rms[1], rms[2])
+                                        rospy.loginfo("(RMS of marker_id is %d) tree_pos errors: [%f, %f], radius_errors: [%f]", tree_db_list[j].marker_id, rms[0], rms[1], rms[2])
 
                                 start_flag = False
 
                                 pose_cnt = 0
-                                tree_squared_errors_sum = [0] * 3
+                                tree_squared_errors_sum_list = [[0] * 3] * len(tree_squared_errors_sum_list)
 
                         else:
                                 if (key == '\x03'):
