f82d60c51aa3704b54b805b72ff33cfa9b22904c
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-08-11 17:12:42 +0900

    [agile_multirotor][obstacle_visualize] get csv file from argumet

--- robots/agile_multirotor/config/obstacle_world/tree_2dvisualization.py
+++ robots/agile_multirotor/config/obstacle_world/tree_2dvisualization.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import csv
 import matplotlib.pyplot as plt
-def main():
-    body_size = 0.25 #[radius]
+import argparse
+
+def main(csv_file = "real.csv"):
+    body_size = 0.30 #[radius]
     figure, axes = plt.subplots()
-    with open("real.csv", 'r') as f:
+    with open(csv_file, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             x = float(row[1])
@@ -13,8 +15,11 @@ def main():
             draw_circle = plt.Circle((x, y), r)
             axes.add_artist(draw_circle)
     plt.title('ObstacleMap')
-    plt.xlim([-0.2, 4.2])
-    plt.ylim([-1.5, 1.5])
+    plt.xlim([-0.2, 3.2])
+    plt.ylim([-1.95, 1.95])
     plt.show()
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='drow obstacle circle')
+    parser.add_argument('csv_file', type=str, help='CSV file path')
+    args = parser.parse_args()
+    main(args.csv_file)
